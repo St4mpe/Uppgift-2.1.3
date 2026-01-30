89620b8b1bc4e80680b8eff098c6df0f10543261
St4mpe <hampusekmansundin@gmail.com>
2026-01-30 11:36:46 +0100

    1

--- index.php
+++ index.php
@@ -4,11 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <form action="index.php" method="post">
-        <input type="text" name="user" id="user" placeholder="Användarnamn">
-        <input type="password" name="pass" id="pass" placeholder="password">
-    </form>
+    <section class="inloggningsruta">
+        <form action="index.php" method="post">
+            <label for="text">Logga in (Good Good, I Promise)</label>
+            <input class="ruta" type="text" name="user" id="user" placeholder="Användarnamn">
+            <input class="ruta" type="password" name="pass" id="pass" placeholder="Lösenord">
+            <input class="submitbtn" type="submit" value="Submit">
+        </form>
+    </section>
 </body>
 </html>
